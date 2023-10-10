6718930099a910cb1b257960ee476117d8721169
Mendy Green <greenlighttec@users.noreply.github.com>
2023-10-10 00:29:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, make_response, send_from_directory
 import eiscp, json, uuid
 
 def load_settings():
