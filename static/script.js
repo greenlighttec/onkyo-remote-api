c9a938797dc50a199248c2e678c1831b42d84e62
Mendy Green <greenlighttec@users.noreply.github.com>
2023-10-09 21:52:32 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,24 +2,25 @@ async function scanReceivers() {
     const response = await fetch('/api/scan');
     const data = await response.json();
     const select = document.getElementById('receiverList');
-    select.innerHTML = '';
-    data.forEach(ip => {
+    select.innerHTML = '<option value="">--Select Receiver--</option>';
+    
+    if (data.length === 0) {
         const option = document.createElement('option');
-        option.value = ip;
-        option.text = ip;
+        option.value = "";
+        option.text = "No results found";
+        option.disabled = true;
         select.appendChild(option);
-    });
-}
-
-async function sendCommand(command) {
-    const ip = document.getElementById('receiverList').value;
-    const response = await fetch('/api/command', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ ip, command })
-    });
-    const data = await response.json();
-    console.log(data);
+    } else {
+        data.forEach(ip => {
+            const option = document.createElement('option');
+            option.value = ip;
+            option.text = ip;
+            select.appendChild(option);
+        });
+    }
+    
+    const manualOption = document.createElement('option');
+    manualOption.value = "manual";
+    manualOption.text = "Specify IP Manually";
+    select.appendChild(manualOption);
 }
